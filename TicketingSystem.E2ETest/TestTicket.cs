216aeb4a4ef6b39368dab7eb6cebff0bdc852b20
DangMH <DangMH@gmail.com>
2014-12-21 04:37:08 -0500

    Adding missing test solution files

---
+++ TicketingSystem.E2ETest/TestTicket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TicketingSystem.E2ETest
+{
+    public class TestTicket
+    {
+        private static Random random = new Random((int)DateTime.Now.Ticks);
+        private static HashSet<int> sections = new HashSet<int>();
+        private static HashSet<int> rows = new HashSet<int>();
+        private static HashSet<int> seats = new HashSet<int>();
+
+        /// <summary>
+        /// Generates a random ticket
+        /// </summary>
+        public TestTicket()
+        {
+            while (sections.Contains(section = random.Next())) ;
+            sections.Add(section);
+
+            while (rows.Contains(row = random.Next()));
+            rows.Add(row);
+
+            while (seats.Contains(seat = random.Next()));
+            seats.Add(seat);
+        }
+
+        public int section { get; set; }
+
+        public int row { get; set; }
+
+        public int seat { get; set; }
+    }
+}
---
+++ TicketingSystem.E2ETest/TicketingSystem.E2ETest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TicketingSystem.E2ETest
+{
+    public class E2ETest
+    {
+        public static void Main(string[] args)
+        {
+            List<TestMember> memberList = null;
+            List<TestTicket> ticketList = null;
+
+            Console.WriteLine("Enter the number of concurrent test members: ");
+            int numMembers = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the number of test tickets: ");
+            int numTickets = Convert.ToInt32(Console.ReadLine());
+
+            memberList = new List<TestMember>(numMembers);
+            ticketList = new List<TestTicket>(numTickets);
+
+            Console.WriteLine("Member List:");
+            for (int i = 0; i < numMembers; ++i)
+            {
+                TestMember tempMember = new TestMember();
+                memberList.Add(tempMember);
+
+                Console.WriteLine("\t- " + tempMember.name);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Ticket List:");
+            for (int i = 0; i < numMembers; ++i)
+            {
+                TestTicket tempTicket = new TestTicket();
+                ticketList.Add(tempTicket);
+
+                Console.WriteLine("\t- " + tempTicket.section + " " + tempTicket.row + " " + tempTicket.seat);
+            }
+            Console.WriteLine();
+
+            RunTest(memberList, ticketList);
+
+            Console.WriteLine("Enter Any Key to Exit");
+            Console.Read();
+        }
+
+        public async static void RunTest(List<TestMember> memberList, List<TestTicket> ticketList)
+        {
+        }
+    }
+}
